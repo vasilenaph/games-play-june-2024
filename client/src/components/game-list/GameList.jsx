4a7a6138e661086d7d54b2bb460c787c2b41c51c
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2024-07-18 11:05:35 +0300

    Add no games message

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -13,12 +13,12 @@ export default function GameList() {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {/* <!-- Display div: with information about every game (if any) --> */}
 
-            {games.map(game => <GameListItem key={game._id} {...game} />)}
+            {games.length > 0
+                ? games.map(game => <GameListItem key={game._id} {...game} />)
+                : <h3 className="no-articles">No articles yet</h3>
+            }
 
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            <h3 className="no-articles">No articles yet</h3>
         </section>
     );
 }
